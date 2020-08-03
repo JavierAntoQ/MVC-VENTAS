005ea53c43b0936a9dbba564f73183e0c6cc3b00
JavierQuispe <1813011329@UNTELS.EDU.PE>
2020-08-03 12:10:16 -0500

    MVC SUGERENCIA MODELO.JAVA TERMINADO

--- src/java/Modelo/Ventas.java
+++ src/java/Modelo/Ventas.java
@@ -10,5 +10,59 @@ package Modelo;
  * @author USER
  */
 public class Ventas {
+    private int ideproducto;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    private String estado;
+
+    public Ventas(int ideproducto, String nombre, double precio, int cantidad, String estado) {
+        this.ideproducto = ideproducto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.estado = estado;
+    }
+
+    public int getIdeproducto() {
+        return ideproducto;
+    }
+
+    public void setIdeproducto(int ideproducto) {
+        this.ideproducto = ideproducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
 }
